232d62fc1ec28c4296ba583c76525da0c67a87fa
suplzzz <dualspeed34@gmail.com>
2024-01-23 21:52:28 +0300

    KS-1-5

---
+++ src/main/kotlin/lesson1/task5.kt
@@ -0,0 +1,10 @@
+package org.example.lesson1
+
+fun main(){
+    var seconds = 6480
+    val hours = seconds / 3600
+    val minutes = seconds / 60 - 60 * hours
+    seconds = seconds - 3600 * hours - 60 * minutes
+
+    println("${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}")
+}
