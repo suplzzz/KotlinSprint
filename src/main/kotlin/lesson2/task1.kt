2c60b912770511b038c54cf084638be4d9cee65a
suplzzz <dualspeed34@gmail.com>
2024-01-24 19:20:51 +0300

    KS-2-1

---
+++ src/main/kotlin/lesson2/task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson2
+
+fun main() {
+    val numberOfStudents = 4
+
+    val gradeOfStudent1 = 3.0
+    val gradeOfStudent2 = 4.0
+    val gradeOfStudent3 = 3.0
+    val gradeOfStudent4 = 5.0
+
+    val averageGrade = (gradeOfStudent1 + gradeOfStudent2 + gradeOfStudent3 + gradeOfStudent4) / numberOfStudents
+
+    println(averageGrade)
+}
