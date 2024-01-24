80c7d10e4e5f4cdc70baaa66b2b6de9412a2991e
suplzzz <dualspeed34@gmail.com>
2024-01-24 15:52:58 +0300

    KS-1-5 fix

--- src/main/kotlin/lesson1/task5.kt
+++ src/main/kotlin/lesson1/task5.kt
@@ -1,14 +1,14 @@
 package org.example.lesson1
 
 fun main() {
-    var seconds = 6480
-    val hours = seconds / 3600
-    val minutes = seconds / 60 - 60 * hours
-    seconds = seconds - 3600 * hours - 60 * minutes
+    val oneMinute = 60
+    val oneHour = 3600
 
-    println(
-        "${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${
-            seconds.toString().padStart(2, '0')
-        }"
-    )
+    var seconds = 6480
+    val hours = seconds / oneHour
+    val minutes = seconds / oneMinute - oneMinute * hours
+
+    seconds = seconds - oneHour * hours - oneMinute * minutes
+
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
