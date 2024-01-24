1554e613aeca6ab83a4265e55edf0d9a76ba7788
suplzzz <dualspeed34@gmail.com>
2024-01-24 15:29:05 +0300

    KS-1-2 fix

--- src/main/kotlin/lesson1/task2.kt
+++ src/main/kotlin/lesson1/task2.kt
@@ -1,17 +1,17 @@
 package org.example.lesson1
 
-fun main(){
-    var numberOfOrders: Int = 75
+fun main() {
+    val numberOfOrders: Int = 75
     val thanksText: String = "Thank you for your purchase!"
 
     println(numberOfOrders)
     println(thanksText)
 
-   /* var numberOfStaff: Int = 2000
+    var numberOfStaff: Int = 2000
 
-    println(numberOfStaff) */
+    //println(numberOfStaff)
 
-    var numberOfStaff: Int = 1999
+    numberOfStaff = 1999
 
     println(numberOfStaff)
 
