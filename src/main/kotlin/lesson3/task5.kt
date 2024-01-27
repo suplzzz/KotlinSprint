5038fee53c5a9458bb824c50fa69316de92d24f4
suplzzz <dualspeed34@gmail.com>
2024-01-27 10:53:29 +0300

    KS-3-5 fix with .split

--- src/main/kotlin/lesson3/task5.kt
+++ src/main/kotlin/lesson3/task5.kt
@@ -3,9 +3,11 @@ package org.example.lesson3
 fun main(){
     val input = "D2-D4;0"
 
-    val start = input.substring(0, 2)
-    val end = input.substring(3,5)
-    val moveNumber = input.substring(6)
+    val output = input.split("-", ";")
+
+    val start = output[0]
+    val end = output[1]
+    val moveNumber = output[2]
 
     println(start)
     println(end)
