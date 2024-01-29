b0ed792ceaffa863cce470829382fc4cbb17df0d
suplzzz <dualspeed34@gmail.com>
2024-01-29 22:15:37 +0300

    KS-4-3 FIX

--- src/main/kotlin/lesson4/task3.kt
+++ src/main/kotlin/lesson4/task3.kt
@@ -1,11 +1,6 @@
 package org.example.lesson4
 
 fun main() {
-    val requirement1: Boolean = true
-    val requirement2: Boolean = true
-    val requirement3: Int = 20
-    val requirement4: String = "зима"
-
     val condition1: Boolean = true
     val condition2: Boolean = true
     val condition3: Int = 20
@@ -13,8 +8,15 @@ fun main() {
 
     println(
         "Благоприятные ли сейчас условия для роста бобовых? ${
-            (condition1 == requirement1) and
-                    (condition2 == requirement2) and (condition3 == requirement3) and (condition4 != requirement4)
+            condition1 == REQUIREMENT1 &&
+                    condition2 == REQUIREMENT2 &&
+                    condition3 == REQUIREMENT3 &&
+                    condition4 != REQUIREMENT4
         }"
     )
 }
+
+const val REQUIREMENT1: Boolean = true
+const val REQUIREMENT2: Boolean = true
+const val REQUIREMENT3: Int = 20
+const val REQUIREMENT4: String = "зима"
