811d26e59c5964d70c6475359f653ff19c37c1d6
suplzzz <dualspeed34@gmail.com>
2024-01-29 22:42:15 +0300

    KS-4-5 FIX

--- src/main/kotlin/lesson4/task5.kt
+++ src/main/kotlin/lesson4/task5.kt
@@ -1,21 +1,24 @@
 package org.example.lesson4
 
 fun main() {
-    val requirement1 = true
-    val lowerRequirement2 = 55
-    val upperRequirement2 = 70
-    val requirement3 = 50
-    val requirement4 = true
-
     val condition1 = readln().toBoolean()
     val condition2 = readln().toInt()
     val condition3 = readln().toInt()
     val condition4 = readln().toBoolean()
 
     val readiness =
-        ((condition1 == requirement1) and (condition2 in lowerRequirement2..upperRequirement2) and
-                (condition3 >= requirement3)) or
-                ((condition2 == upperRequirement2) and (condition4 == requirement4) and (condition3 >= requirement3))
+        (condition1 == REQUIREMENT1 &&
+                condition2 in LOWER_REQUIREMENT2..UPPER_REQUIREMENT2 &&
+                condition3 >= REQUIREMENT3) ||
+                (condition2 == UPPER_REQUIREMENT2 &&
+                        condition4 == REQUIREMENT4 &&
+                        condition3 >= REQUIREMENT3)
 
     println("Ready to voyage: $readiness")
 }
+
+const val REQUIREMENT1 = true
+const val LOWER_REQUIREMENT2 = 55
+const val UPPER_REQUIREMENT2 = 70
+const val REQUIREMENT3 = 50
+const val REQUIREMENT4 = true
