b039e57e67955703e31a33daef16d5007d6621b0
suplzzz <dualspeed34@gmail.com>
2024-01-31 01:16:05 +0300

    KS-5-3

---
+++ src/main/kotlin/lesson5/task3.kt
@@ -0,0 +1,30 @@
+package org.example.lesson5
+
+fun main() {
+    val number1 = (0..42).random()
+    val number2 = (0..42).random()
+
+    println(
+        """
+        Чтобы выиграть приз отгадайте загаданные числа: 2 числа от 0 до 42.
+        Одно число - утешительный приз.
+        Оба числа - главный приз.
+    """.trimIndent()
+    )
+
+    val input1 = readln().toInt()
+    val input2 = readln().toInt()
+
+    if ((input1 == number1 || input1 == number2) && (input2 == number1 || input2 == number2)) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (((input1 == number1 || input1 == number2) && (input2 != number1 && input2 != number2)) ||
+        ((input1 != number1 && input1 != number2) && (input2 == number1 || input2 == number2))
+    ) {
+        println("Вы выиграли утешительный приз!")
+    } else {
+        println("Неудача")
+    }
+
+    println("Были загаданы числа: $number1 и $number2")
+
+}
